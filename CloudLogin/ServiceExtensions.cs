a7c9ad84c499faac6f1670c1dd62e9141b7cfc8d
AngryRamYG <RamiGerges@angrymonkeyagency.com>
2022-09-12 16:26:03 +0300

    Linked to CosmosDB and added Google identity

--- CloudLogin/ServiceExtensions.cs
+++ CloudLogin/ServiceExtensions.cs
@@ -1,6 +1,7 @@
 ﻿//using Microsoft.Extensions.DependencyInjection;
 using AngryMonkey.Cloud.Components;
 using AngryMonkey.Cloud.Login;
+using AngryMonkey.Cloud.Login.DataContract;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Http;
@@ -17,55 +18,79 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public class CloudLoginService
 {
-	IServiceCollection AddCloudLogin { get; }
-	public CloudLoginConfiguration Options { get; set; }
+    IServiceCollection AddCloudLogin { get; }
+    public CloudLoginConfiguration Options { get; set; }
 }
 
 public static class MvcServiceCollectionExtensions
 {
 
-	public static CloudLoginService AddCloudLogin(this IServiceCollection services, CloudLoginConfiguration options)
-	{
-		services.AddSingleton(new CloudLoginService() { Options = options });
-		//services.AddSingleton<CloudLoginProcess>();
+    public static CloudLoginService AddCloudLogin(this IServiceCollection services, CloudLoginConfiguration options)
+    {
+        services.AddSingleton(new CloudLoginService() { Options = options });
+        //services.AddSingleton<CloudLoginProcess>();
 
-		var service = services.AddAuthentication("Cookies").AddCookie(option =>
-		{
-			option.Cookie.Name = "ChatboxAuthentication";
-			option.Events = new AspNetCore.Authentication.Cookies.CookieAuthenticationEvents()
-			{
-				OnSignedIn = async context =>
-				{
-					string? emaillAddress = context.Principal?.FindFirst(ClaimTypes.Email)?.Value;
-				}
-			};
-		});
+        var service = services.AddAuthentication("Cookies").AddCookie(option =>
+        {
+            option.Cookie.Name = "ChatboxAuthentication";
+            option.Events = new AspNetCore.Authentication.Cookies.CookieAuthenticationEvents()
+            {
+                OnSignedIn = async context =>
+                {
+                    string? emaillAddress = context.Principal?.FindFirst(ClaimTypes.Email)?.Value;
 
-		foreach (CloudLoginConfiguration.Provider provider in options.Providers)
-		{
-			// Microsoft
 
-			if (provider.GetType() == typeof(CloudLoginConfiguration.MicrosoftAccount))
-				service.AddMicrosoftAccount(Option =>
-				{
-					Option.SignInScheme = "Cookies";
-					Option.ClientId = provider.ClientId;
-					Option.ClientSecret = provider.ClientSecret;
-				});
+                    string firstName = context.Principal?.FindFirst(ClaimTypes.GivenName)?.Value;
+                    string lastName = context.Principal?.FindFirst(ClaimTypes.Surname)?.Value;
 
-			// Google
+                    AngryMonkey.Cloud.Login.DataContract.User user = new(firstName, lastName)
+                    {
+                        PartitionKey = "Users",
+                        ID = Guid.NewGuid(),
+                        DisplayName = context.Principal?.FindFirst(ClaimTypes.Name)?.Value,
+                        EmailAddresses = new()
+                        {
+                            new UserEmailAddress()
+                            {
+                                EmailAddress = emaillAddress,
+                                IsPrimary = true,
+                                ProviderId = context.Principal?.FindFirst(ClaimTypes.NameIdentifier)?.Value,
+                                Provider = context.Principal.Identity.AuthenticationType
+                            }
+                        }
+                    };
+                    options.Cosmos.Methods.Container.CreateItemAsync(user);
 
-			if (provider.GetType() == typeof(CloudLoginConfiguration.GoogleAccount))
-				service.AddGoogle(Option =>
-				{
-					Option.SignInScheme = "Cookies";
-					Option.ClientId = provider.ClientId;
-					Option.ClientSecret = provider.ClientSecret;
-				});
-		}
+                    Console.WriteLine(user.Username);
+                }
+            };
+        });
 
-		return null;
-	}
+        foreach (CloudLoginConfiguration.Provider provider in options.Providers)
+        {
+            // Microsoft
+
+            if (provider.GetType() == typeof(CloudLoginConfiguration.MicrosoftAccount))
+                service.AddMicrosoftAccount(Option =>
+                {
+                    Option.SignInScheme = "Cookies";
+                    Option.ClientId = provider.ClientId;
+                    Option.ClientSecret = provider.ClientSecret;
+                });
+
+            // Google
+
+            if (provider.GetType() == typeof(CloudLoginConfiguration.GoogleAccount))
+                service.AddGoogle(Option =>
+                {
+                    Option.SignInScheme = "Cookies";
+                    Option.ClientId = provider.ClientId;
+                    Option.ClientSecret = provider.ClientSecret;
+                });
+        }
+
+        return null;
+    }
 }
 
 //public class CloudLoginProcess
@@ -77,39 +102,39 @@ public static class MvcServiceCollectionExtensions
 
 public class CloudLoginConfiguration
 {
-	public List<Provider> Providers { get; set; } = new();
-	public CosmosDatabase? Cosmos { get; set; }
+    public List<Provider> Providers { get; set; } = new();
+    public CosmosDatabase? Cosmos { get; set; }
 
-	public class Provider
-	{
-		public string Code { get; set; }
-		public string ClientId { get; set; }
-		public string ClientSecret { get; set; }
-	}
+    public class Provider
+    {
+        public string Code { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
 
-	public class MicrosoftAccount : Provider
-	{
-		public MicrosoftAccount()
-		{
-			Code = "Microsoft";
-		}
-	}
+    public class MicrosoftAccount : Provider
+    {
+        public MicrosoftAccount()
+        {
+            Code = "Microsoft";
+        }
+    }
 
-	public class GoogleAccount : Provider
-	{
-		public GoogleAccount()
-		{
-			Code = "Google";
-		}
-	}
+    public class GoogleAccount : Provider
+    {
+        public GoogleAccount()
+        {
+            Code = "Google";
+        }
+    }
 
-	public class CosmosDatabase
-	{
-		public string ConnectionString { get; set; }
-		public string DatabaseId { get; set; }
-		public string ContainerId { get; set; }
+    public class CosmosDatabase
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseId { get; set; }
+        public string ContainerId { get; set; }
 
-		private CosmosMethods? methods = null;
-		internal CosmosMethods Methods => methods ??= new CosmosMethods(ConnectionString, DatabaseId, ContainerId);
-	}
+        private CosmosMethods? methods = null;
+        internal CosmosMethods Methods => methods ??= new CosmosMethods(ConnectionString, DatabaseId, ContainerId);
+    }
 }

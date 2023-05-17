6f84bc3f8ad199fff60d2f9581cf2a3b45fe2489
AngryRamYG <RamiGerges@angrymonkeyagency.com>
2023-05-17 16:22:56 +0300

    fixing mvc logout version 0.0.29

--- CloudLogin.MVC/CloudLoginController.cs
+++ CloudLogin.MVC/CloudLoginController.cs
@@ -92,7 +92,7 @@ public class CloudLoginController : ControllerBase
 
         //Response.Cookies.Delete("User");
         //Response.Cookies.Delete("LoggedInUser");
-        Response.Cookies.Delete("CloudLogin");
+        //Response.Cookies.Delete("CloudLogin");
         var baseUri = $"{Request.Scheme}://{Request.Host}";
 
         string seperator = CloudLogin.LoginUrl.Contains('?') ? "&" : "?";
